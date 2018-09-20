6483e4c77a13afc41df76df43dc635c9b0eada37
umi <0123mark3210@gmail.com>
2018-09-20 09:57:16 +0900

    mobile access/contactのstyle修正

--- docs/javascript/main.js
+++ docs/javascript/main.js
@@ -60,7 +60,7 @@ function init() {
 		targets: '#lineDrawing .lines path',
 		strokeDashoffset: [anime.setDashoffset, 0],
 		easing: 'easeInOutSine',
-		duration: 1500,
+		duration: 3000,
 		delay: function (el, i) {
 			return i * 250
 		},
