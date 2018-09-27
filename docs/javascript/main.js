93deebb8b61be54c68cd857972f0c2c95e33feb1
umi <0123mark3210@gmail.com>
2018-09-27 19:31:58 +0900

    fix code

--- docs/javascript/main.js
+++ docs/javascript/main.js
@@ -1,46 +1,64 @@
-window.addEventListener('load', () => {
-	// service worker
-	if ('serviceWorker' in navigator) {
-		navigator.serviceWorker.register('/tokyo-underground-stadium-demo/serviceWorker.js', {
-				scope: '/tokyo-underground-stadium-demo/'
-			})
-			.then((registration) => {
-				console.log('ServiceWorker registration successful with scope: ', registration.scope);
-			}).catch((err) => {
-				console.log('ServiceWorker registration failed: ', err);
-			});
-	}
-
-	// contactのLogoAnimation
-	var lineDrawing = anime({
-		targets: '#lineDrawing .lines path',
-		strokeDashoffset: [anime.setDashoffset, 0],
-		easing: 'easeInOutSine',
-		duration: 3000,
-		delay: function (el, i) {
-			return i * 250
-		},
-		direction: 'alternate',
-		loop: false
+document.addEventListener("DOMContentLoaded", () => {
+	// cookie
+	var doOnce = (() => {
+		if (document.cookie.replace(/(?:(?:^|.*;\s*)doSomethingOnlyOnce\s*\=\s*([^;]*).*$)|^.*$/, "$1") !== "true") {
+			document.cookie = "doSomethingOnlyOnce=true; expires=Fri, 31 Dec 9999 23:59:59 GMT";
+			return Boolean(true)
+		} else {
+			return Boolean(false);
+		}
 	});
-
-	// indexのSlideShow
-	var elem = document.querySelector('.main-carousel');
-	if (elem != null) {
-		var flicky = new Flickity(elem, {
-			cellAlign: 'center',
-			contain: true,
-			autoPlay: 3000
-		});
+	
+	// index load画面（Animationだけ）
+	const progressLogEl = document.querySelector('#update .progress-log');
+	if (progressLogEl != null) {
+		if (doOnce()) {
+			// stored cookie
+			const easingName = 'easeInOutQuad'
+			var progress = anime({
+				targets: '#callbacks .el',
+				translateX: 250,
+				delay: 1000,
+				easing: easingName,
+				update: function (anim) {},
+				complete: function (anim) {
+					// fading animation
+					var fade = anime({
+						targets: '.loading',
+						opacity: 0,
+						delay: 1000,
+						easing: easingName,
+						complete: function (anim) {
+							console.log('animation completed');
+							document.querySelector('.noScroll').classList.remove("noScroll");
+							const loading = $(".loading");
+							loading.css("display", "none");
+						}
+					});
+				}
+			});
+	
+			var progress_bar = anime({
+				targets: '.progress-bar',
+				delay: 1000,
+				easing: easingName,
+				width: '200px',
+			});
+		} else {
+			// already exist cookie
+			document.querySelector('.noScroll').classList.remove("noScroll");
+			const loading = $(".loading");
+			loading.css("display", "none");
+		}
 	}
-
+	
 	// index more viewのモーフィング
 	const about = document.querySelector('.about');
 	if (about != null) {
 		const button = document.querySelectorAll(".button");
 		const path = document.querySelectorAll(".polymorph");
 		for (let i = 0; i < button.length; i++) {
-
+	
 			button[i].addEventListener('mouseover', () => {
 				anime.remove(path[i]);
 				var hover = anime({
@@ -73,40 +91,20 @@ window.addEventListener('load', () => {
 			});
 		}
 	}
-});
-
-
-$(function () {
-	var $win = $(window);
-	$win.on('load resize', function () {
-		// 画面サイズによって画像を差し替える
-		var windowWidth = window.innerWidth;
-		const aboutImage = $(".about__right").find("img");
-		if (windowWidth > 1000) {
-			// PCの処理
-			aboutImage.attr("src", "./assets/image/Desktop/home-about.jpg");
-
-			// slideshowの削除
-			const slideshow = document.querySelector(".event");
-			if (slideshow != null) {
-				document.querySelector("#mobile").classList.add("displayNone");
-				document.querySelector("#desktop").classList.remove("displayNone");
-
-			}
-		} else {
-			// Mobileの処理
-			aboutImage.attr("src", "./assets/image/Mobile/home-about.jpg");
-
-			// slideshowの追加
-			const slideshow = document.querySelector(".event");
-			if (slideshow != null) {
-				document.querySelector("#mobile").classList.remove("displayNone");
-				document.querySelector("#desktop").classList.add("displayNone");
-
-			}
-		}
+	
+	// contactのLogoAnimation
+	var lineDrawing = anime({
+		targets: '#lineDrawing .lines path',
+		strokeDashoffset: [anime.setDashoffset, 0],
+		easing: 'easeInOutSine',
+		duration: 3000,
+		delay: function (el, i) {
+			return i * 250
+		},
+		direction: 'alternate',
+		loop: false
 	});
-
+	
 	// drawer open
 	const bar = document.querySelector(".header-mobile__menu-icon");
 	if (bar != null) {
@@ -115,6 +113,7 @@ $(function () {
 			$(".header-mobile__mat").toggleClass("fading");
 		});
 	}
+	
 	// draw close
 	const mat = document.querySelector(".header-mobile__mat");
 	if (mat != null) {
@@ -123,9 +122,61 @@ $(function () {
 			$(".header-mobile__mat").toggleClass("fading");
 		});
 	}
+	
+	$(window).on('load resize', () => {
+		// 画面サイズによって画像を差し替える
+		var windowWidth = window.innerWidth;
+		const aboutImage = $(".about__right").find("img");
+		if (windowWidth > 1000) {
+			// PCの処理
+			aboutImage.attr("src", "./assets/image/Desktop/home-about.jpg");
+	
+			// slideshowの削除
+			const slideshow = document.querySelector(".event");
+			if (slideshow != null) {
+				document.querySelector("#mobile").classList.add("displayNone");
+				document.querySelector("#desktop").classList.remove("displayNone");
+			}
+	
+		} else {
+			// Mobileの処理
+			aboutImage.attr("src", "./assets/image/Mobile/home-about.jpg");
+	
+			// slideshowの追加
+			const slideshow = document.querySelector(".event");
+			if (slideshow != null) {
+				document.querySelector("#mobile").classList.remove("displayNone");
+				document.querySelector("#desktop").classList.add("displayNone");
+			}
+		}
+	});
+});
 
-	let curr_scroll_top;
+window.addEventListener('load', () => {
+	// service worker
+	if ('serviceWorker' in navigator) {
+		navigator.serviceWorker.register('/tokyo-underground-stadium-demo/serviceWorker.js', {
+				scope: '/tokyo-underground-stadium-demo/'
+			})
+			.then((registration) => {
+				console.log('ServiceWorker registration successful with scope: ', registration.scope);
+			}).catch((err) => {
+				console.log('ServiceWorker registration failed: ', err);
+			});
+	}
+
+	// indexのSlideShow
+	var elem = document.querySelector('.main-carousel');
+	if (elem != null) {
+		var flicky = new Flickity(elem, {
+			cellAlign: 'center',
+			contain: true,
+			autoPlay: 3000
+		});
+	}
+
 	// update user scroll top
+	let curr_scroll_top;
 	$(window).scroll(function () {
 		curr_scroll_top = $(this).scrollTop();
 		// index.html headerのmenuかぶりを修正
@@ -141,140 +192,41 @@ $(function () {
 		}
 
 		// scroll animation exsample:
+		// スクロールの発火位置を300px上にずらす
 		let offsetY = 300;
 		// index
-		// about
 		if (document.querySelector('.about') != null) {
+			// about
 			if (curr_scroll_top + offsetY >= $(".about").offset().top) {
-				// var cssSelector = anime({
-				// 	targets: '.about',
-				// 	translateX: '100px',
-				// 	opacity: 1,
-				// 	easing: 'easeOutExpo',
-				// 	duration: 500,
-				// });
-				document.querySelector('.about').classList.add('slide');
+				document.querySelector('.about').classList.add('slideAnimation');
 			}
 			// event
 			if ($('#desktop').css('display') != 'none') {
 				if (curr_scroll_top + offsetY >= $('#desktop').find(".event").offset().top) {
-					// var cssSelector = anime({
-					// 	targets: '.event',
-					// 	translateX: '100px',
-					// 	opacity: 1,
-					// 	easing: 'easeOutExpo',
-					// 	duration: 500,
-					// });
-					document.querySelectorAll('.event')[0].classList.add('slide');
-					document.querySelectorAll('.event')[1].classList.add('slide');
+					document.querySelectorAll('.event')[0].classList.add('slideAnimation');
+					document.querySelectorAll('.event')[1].classList.add('slideAnimation');
 				}
 			}
 			if ($('#mobile').css('display') != 'none') {
 				if (curr_scroll_top + offsetY >= $('#mobile').find(".event").offset().top) {
-					// var cssSelector = anime({
-					// 	targets: '.event',
-					// 	translateX: '100px',
-					// 	opacity: 1,
-					// 	easing: 'easeOutExpo',
-					// 	duration: 500,
-					// });
-					document.querySelectorAll('.event')[0].classList.add('slide');
-					document.querySelectorAll('.event')[1].classList.add('slide');
+					document.querySelectorAll('.event')[0].classList.add('slideAnimation');
+					document.querySelectorAll('.event')[1].classList.add('slideAnimation');
 				}
 			}
 			// news
 			if (curr_scroll_top + offsetY >= $(".news").offset().top) {
-				// var cssSelector = anime({
-				// 	targets: '.news',
-				// 	translateX: '100px',
-				// 	opacity: 1,
-				// 	easing: 'easeOutExpo',
-				// 	duration: 500,
-				// });
-				document.querySelector('.news').classList.add('slide');
+				document.querySelector('.news').classList.add('slideAnimation');
 			}
 		}
 		// information
 		if (document.querySelector('.open') != null) {
 			if (curr_scroll_top + offsetY >= $(".attention").offset().top) {
-				// var cssSelector = anime({
-				// 	targets: '.attention',
-				// 	translateX: '100px',
-				// 	opacity: 1,
-				// 	easing: 'easeOutExpo',
-				// 	duration: 500,
-				// });
-				document.querySelector('.attention').classList.add('slide');
+				document.querySelector('.attention').classList.add('slideAnimation');
 
 			}
 			if (curr_scroll_top + offsetY >= $(".link").offset().top) {
-				// var cssSelector = anime({
-				// 	targets: '.link',
-				// 	translateX: '100px',
-				// 	opacity: 1,
-				// 	easing: 'easeOutExpo',
-				// 	duration: 500,
-				// });
-				document.querySelector('.link').classList.add('slide');
+				document.querySelector('.link').classList.add('slideAnimation');
 			}
 		}
 	})
 });
-
-document.addEventListener("DOMContentLoaded", init);
-
-function init() {
-	var doOnce = (() => {
-		if (document.cookie.replace(/(?:(?:^|.*;\s*)doSomethingOnlyOnce\s*\=\s*([^;]*).*$)|^.*$/, "$1") !== "true") {
-			document.cookie = "doSomethingOnlyOnce=true; expires=Fri, 31 Dec 9999 23:59:59 GMT";
-			return Boolean(true)
-		} else {
-			return Boolean(false);
-		}
-	});
-
-	// index load画面（Animationだけ）
-	const progressLogEl = document.querySelector('#update .progress-log');
-	if (progressLogEl != null) {
-		if (doOnce()) {
-			// stored cookie
-			const easingName = 'easeInOutQuad'
-			var update = anime({
-				targets: '#callbacks .el',
-				translateX: 250,
-				delay: 1000,
-				easing: easingName,
-				update: function (anim) {},
-				complete: function (anim) {
-
-					document.querySelector('.noScroll').classList.remove("noScroll");
-
-					var fade = anime({
-						targets: '.loading',
-						opacity: 0,
-						delay: 1000,
-						easing: easingName,
-						complete: function (anim) {
-							const loading = $(".loading");
-							loading.css("display", "none");
-						}
-					});
-				}
-			});
-
-			var update_bar = anime({
-				targets: '.progress-bar',
-				delay: 1000,
-				easing: easingName,
-				width: '200px',
-			});
-		} else {
-			// already exist cookie
-			document.querySelector('.noScroll').classList.remove("noScroll");
-			const loading = $(".loading");
-			loading.css("display", "none");
-		}
-	}
-
-
-}
